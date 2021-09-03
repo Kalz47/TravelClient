f3a5a583d060f56b74265ff66be74a58ace915ac
Chanaka97n <chanaka97n@gmail.com>
2021-09-03 11:08:13 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Client/pages/Home";
+import NavBar from "./Client/components/NavBar";
+import Login from "./Client/pages/Login";
+import Register from "./Client/pages/Register";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/login" exact component={Login}></Route>
+        <Route path="/register" exact component={Register}></Route>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
