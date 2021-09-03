0c66fcbe44ad539677eafdd08279cd68da1efa8a
Chanaka97n <chanaka97n@gmail.com>
2021-09-03 12:21:50 +0530

    added admin routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route } from "react-router-dom";
+// client routes
 import Home from "./Client/pages/Home";
 import NavBar from "./Client/components/NavBar";
 import Login from "./Client/pages/Login";
 import Register from "./Client/pages/Register";
+// admin routes
+import AdminLogin from "./Admin/pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <div className="container">
         <Route path="/" exact component={Home}></Route>
         <Route path="/login" exact component={Login}></Route>
         <Route path="/register" exact component={Register}></Route>
+        {/* Admin routes */}
+        <Route path="/admin/login" exact component={AdminLogin}></Route>
       </div>
     </BrowserRouter>
   );
