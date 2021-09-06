ed30e366906c265a46e9e470fe6a4ca6ae92260d
Chanaka97n <chanaka97n@gmail.com>
2021-09-06 14:40:22 +0530

    added login page

--- src/Client/pages/login.js
+++ src/Client/pages/login.js
@@ -5,6 +5,7 @@ import NavBar from "../components/NavBar";
 import { login } from "../../actions/auth";
 import { connect, useDispatch } from "react-redux";
 import PropTypes from "prop-types";
+import Footer from "./HomePage/Footer";
 
 const Login = ({ history }) => {
   const dispatch = useDispatch();
@@ -30,9 +31,9 @@ const Login = ({ history }) => {
   };
 
   return (
-    <div>
+    <div className="bg-hero-pattern bg-no-repeat bg-cover bg-center blur-lg">
       <NavBar />
-      <div className="md:px-40 sm:px-20 px-10">
+      {/* <div className="md:px-40 sm:px-20 px-10">
         <div className="flex h-screen ">
           <div className="m-auto">
             <div className="border border-gray-200 rounded-lg">
@@ -87,7 +88,68 @@ const Login = ({ history }) => {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="md:px-40 sm:px-20 px-10 py-44">
+        <div className="shadow-lg sm:flex">
+          <div
+            className="sm:w-2/5 w-full bg-pink-500 bg-cover bg-center text-white"
+            // style="background-image: url('')"
+          >
+            <div className="p-8">
+              <h1>
+                SIGN<span className="text-white">UP</span>
+              </h1>
+              <p className="leading-tight mt-2 text-sm text-white">
+                Create an account to join our awesome community
+              </p>
+            </div>
+          </div>
+          <div className="sm:w-3/5 w-full bg-white">
+            <div className="p-8">
+              <form onSubmit={handleSubmit}>
+                <label for="username" className="text-xs text-gray-500">
+                  Username
+                </label>
+                <input
+                  className="w-full bg-white rounded-full border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+                <label id="passowrd" className="text-xs text-gray-500">
+                  Password
+                </label>
+                <input
+                  className=" mb-6 w-full bg-white rounded-full border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+
+                <button
+                  // type="submit"
+                  // onClick={handleSubmit}
+                  className="shadow-lg pt-3 pb-3 w-full text-white bg-pink-500 hover:bg-white hover:text-pink-500 rounded-full cursor-pointer border hover:border-pink-500"
+                >
+                  Login
+                </button>
+              </form>
+              <div className="text-center mt-4">
+                <p className="text-sm text-gray-500">
+                  Already have an account?{" "}
+                  <a
+                    href="#"
+                    className="no-underline text-pink-500 font-bold hover:text-pink-400"
+                  >
+                    Sign in
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-pattern":
+          "url('https://images.unsplash.com/photo-1432847712612-926caafaa802?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80')",
+      }),
+    },
   },
   variants: {
     extend: {},
