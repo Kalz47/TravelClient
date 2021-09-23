3c0fccdee542c9d411915dc0f1e58c08768b8a25
Chanaka97n <chanaka97n@gmail.com>
2021-09-23 21:25:23 +0530

    added slider

--- src/Client/pages/HomePage/Welcome.js
+++ src/Client/pages/HomePage/Welcome.js
@@ -3,9 +3,12 @@ import Typed from "react-typed";
 import image1 from "../../images/services/SingleService/productLogos/4ever.jpg";
 import image2 from "../../images/services/SingleService/productLogos/dreamron.jpg";
 import LocationDropDownButton from "../../components/buttons/LocationDropDownButton";
-import Slider from "../../components/slider/Slider";
 import { useDispatch } from "react-redux";
 import { SET_LOCATION, REMOVE_LOCATION } from "../../../actions/type";
+import Slider from "react-slick";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Welcome({ Location, setlocation }) {
   const dispatch = useDispatch();
@@ -21,13 +24,13 @@ export default function Welcome({ Location, setlocation }) {
   }, [Location]);
 
   return (
-    <div className="md:flex md:h-screen  md:mr-0 mr-10 w-full h-60">
+    <div className="md:flex md:h-screen  md:mr-0 mr-10 w-full h-96">
       <div className="md:flex items-center md:pl-16 pl-4 space-x-16">
         <div className="mt-12">
           <h1 className="lg:text-5xl text-sitetheme-blue  font-bold leading-tight text-3xl">
             eCrew Digital Lanka{" "}
           </h1>
-          <p className="mt-4 text-lg font-normal text-sitetheme-blue">
+          <p className="mt-4 text-lg font-normal text-sitetheme-blue md:w-96">
             <Typed
               strings={[
                 "Hello, we’re the eCrew, and we’re ready to serve you!",
@@ -48,8 +51,30 @@ export default function Welcome({ Location, setlocation }) {
         </div>
       </div>
       {/* <!-- Box right --> */}
-      <div className=" w-screen my-4">
-        <Slider />
+      <div className=" md:w-full w-80 my-4 h-screen">
+        {" "}
+        <Slider
+          dots={true}
+          infinite={true}
+          speed={500}
+          slidesToShow={1}
+          slidesToScroll={1}
+          centerMode={true}
+          // centerPadding={100}
+        >
+          <div>
+            <img className="w-96" src={image1} />
+          </div>
+          <div>
+            <img src={image1} />
+          </div>
+          <div>
+            <img src={image1} />
+          </div>
+          <div>
+            <img src={image1} />
+          </div>
+        </Slider>
       </div>
     </div>
   );
