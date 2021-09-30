6a2d025819012e7edd3e36d7b62796d9eb8f2fd2
Chanaka97n <chanaka97n@gmail.com>
2021-09-30 14:38:55 +0530

    added

--- src/Admin/components/BookingDetails.js
+++ src/Admin/components/BookingDetails.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { cartDone, cartInProgress, getCart } from "../../actions/cart";
 
 export default function BookingDetails({ index, c }) {
